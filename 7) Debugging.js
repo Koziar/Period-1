19fc66748a7207e3089a496e112226b729c4e457
LukaszKoziarski <lkoziars@gmail.com>
2016-05-20 17:41:09 +0200

    objects in js - debug

--- 7) Debugging.js
+++ 7) Debugging.js
@@ -1,3 +1,10 @@
-/**
- * Created by Luke on 15.02.2016.
- */
+// All JavaScript objects inherit the properties and methods from their prototype.
+// Therefore it can shove what the day and time you ran the program.
+
+var a = {};
+var b = [];
+var c = true;
+var d = "Hello World";
+var e = 123;
+var f = new Date();
+console.log('Add a breakpoint on me');
